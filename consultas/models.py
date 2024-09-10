648763a3af73d67fd7870727a5298c2e567aabaf
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-09-09 21:15:15 -0300

    .
    .

--- consultas/models.py
+++ consultas/models.py
@@ -7,6 +7,7 @@ class Consulta(models.Model):
     descripcion = models.TextField()
     fecha = models.DateField(auto_now_add=True)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    respuesta = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.descripcion
