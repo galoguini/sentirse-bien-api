ff1cf4ce886c9194cf19302641bbfbcd7aff3cac
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-09-14 15:42:15 -0300

    turnos y railway config

--- turnos/urls.py
+++ turnos/urls.py
@@ -1,5 +1,7 @@
-
+from django.urls import path
+from .views import ElegirTurno, VerTurnos
 
 urlpatterns = [
-    
+    path('elegir_turno/', ElegirTurno.as_view()),
+    path('obtener_turnos/', VerTurnos.as_view()),
 ]
