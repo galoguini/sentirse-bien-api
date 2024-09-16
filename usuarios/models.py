368f91d14c3753f1bed6e30370bdf1c76de62764
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-09-16 17:19:01 -0300

    .
    .

--- usuarios/models.py
+++ usuarios/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     telefono = models.CharField(max_length=20)
-    rol = models.IntegerField()
+    rol = models.IntegerField(blank=True, null=True, default=0)
 
     def __str__(self):
         return self.email
