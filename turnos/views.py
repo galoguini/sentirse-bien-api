212f30d53b6823f665077020594f1eb1204539ee
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-10-27 17:56:39 -0300

    .

--- turnos/views.py
+++ turnos/views.py
@@ -2,13 +2,14 @@ from rest_framework import generics
 from .models import Turno
 from .serializers import TurnoSerializer
 from rest_framework.permissions import IsAuthenticated
+from usuarios.models import Usuario
+import random
 
 class TurnoListView(generics.ListAPIView):
     serializer_class = TurnoSerializer
     permission_classes = [IsAuthenticated]  
     def get_queryset(self):
         usuario = self.request.user  
-
         if usuario.rol >= 1:  
             return Turno.objects.all()  
         else:  
@@ -17,6 +18,9 @@ class TurnoListView(generics.ListAPIView):
 class TurnoCreateView(generics.CreateAPIView):
     queryset = Turno.objects.all()
     serializer_class = TurnoSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(usuario=self.request.user)
+        profesionales = Usuario.objects.filter(rol=1)
+        profesional_random = random.choice(profesionales) if profesionales.exists() else None
+        serializer.save(cliente=self.request.user, profesional=profesional_random)
