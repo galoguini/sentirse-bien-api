81b47dac248b255083b6ec4b7431f5e1065fa75d
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-10-27 17:45:17 -0300

    .

--- turnos/models.py
+++ turnos/models.py
@@ -8,10 +8,8 @@ class Turno(models.Model):
     servicio = models.CharField(max_length=100)
     cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='turnos_como_cliente', null=True, blank=True)
     profesional = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='turnos_como_profesional', null=True, blank=True)
+    pagado = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.fecha} - {self.servicio}'
 
-    @property
-    def pagado(self):
-        return Pago.objects.filter(turno=self).exists()
--- turnos/serializers.py
+++ turnos/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Turno
 
 class TurnoSerializer(serializers.ModelSerializer):
-    pagado = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Turno
