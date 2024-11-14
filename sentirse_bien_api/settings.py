7b4e0a112e5c1a673267ee0037dc62005072d5ce
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-11-13 22:14:44 -0300

    .

--- sentirse_bien_api/settings.py
+++ sentirse_bien_api/settings.py
@@ -149,6 +149,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+# CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS')]
